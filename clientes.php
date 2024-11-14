ed98d6e2d0f373dfb287e11acab36418ad506009
NicolasZGH <147265658+NicolasZGH@users.noreply.github.com>
2024-11-14 15:00:36 -0300

    Update clientes.php

--- clientes.php
+++ clientes.php
@@ -22,10 +22,10 @@ echo "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
 echo "<title>Clientes</title>";
 echo "<link rel='stylesheet' href='style/inicio.css'>";
 echo "<link rel='stylesheet' href='style/navbar.css'>";
-echo "</head>";
+echo "<link rel='icon' href='images/icon.png'>";
 echo "<body>";
 
-// Barra de navegación
+#Barra de navegación
 echo "<div class='navbar'>";
 echo "<a href='inicio.php?section=inicio'>Inicio</a>";
 echo "<a href='pedidos.php?section=pedidos'>Agregar Pedidos</a>";
@@ -37,12 +37,39 @@ echo "</div>";
 
 echo "<div class='content'>";
 
-// Sección de Clientes
-if ($section == 'clientes') {
-    // Mostrar clientes de la tabla
+#Contenedor de centrado para la búsqueda de clientes
+echo "<div style='display: flex; justify-content: center; margin-bottom: 1rem;'>";
+
+#Formulario de búsqueda de clientes
+echo "<div class='busqueda-cliente' style='background-color: #f2f2f2; padding: 1rem; border-radius: 0.5rem; width: 500px;'>";
+echo "<form method='GET' action='clientes.php' style='display: flex; align-items: center;'>";
+echo "<label for='cliente' style='margin-right: 0.5rem;'>Buscar cliente:</label>";
+echo "<input type='text' id='cliente' name='cliente' placeholder='Nombre del cliente' style='padding: 0.5rem; border: 1px solid #ccc; border-radius: 0.25rem; width: 200px; font-size: 0.9rem;'>";
+echo "<button type='submit' style='padding: 0.5rem 1rem; background-color: #e64e08; color: #fff; border: none; border-radius: 0.25rem; cursor: pointer; font-size: 0.9rem; margin-left: 0.5rem;'>Buscar</button>";
+
+#Botón "Volver" que redirige a la misma página sin parámetros de búsqueda
+echo "<button href='clientes.php' style='padding: 0.5rem 1rem; background-color: #333; color: #fff; border: none; border-radius: 0.25rem; cursor: pointer; font-size: 0.9rem; margin-left: 0.5rem;'>Volver</button>";
+
+echo "</form>";
+echo "</div>";
+echo "</div>";
+
+#Verificar si se ingresó un nombre de cliente en el formulario de búsqueda
+if (isset($_GET['cliente']) && !empty($_GET['cliente'])) {
+    $cliente = "%" . $_GET['cliente'] . "%"; #Añadir comodines para búsqueda parcial
+    $sql_clientes = "SELECT idClientes, Nombre, Apellido, Email, Direccion, Telefono, DNI FROM clientes WHERE Nombre LIKE ?";
+    $stmt = $conn->prepare($sql_clientes);
+    $stmt->bind_param("s", $cliente);
+    $stmt->execute();
+    $result_clientes = $stmt->get_result();
+} else {
+    #Mostrar todos los clientes si no se busca un cliente específico
     $sql_clientes = "SELECT idClientes, Nombre, Apellido, Email, Direccion, Telefono, DNI FROM clientes";
     $result_clientes = $conn->query($sql_clientes);
+}
 
+#Sección de Clientes
+if ($section == 'clientes') {
     if ($result_clientes->num_rows > 0) {
         echo "<h2>Tabla de Clientes</h2>";
         echo "<table>";
@@ -57,10 +84,10 @@ if ($section == 'clientes') {
             echo "<td>" . $row["DNI"] . "</td>";
             echo "<td>";
             
-            // Botón para editar el cliente
-            echo "<a href='clientes.php?section=editar&cliente_id=" . $row["idClientes"] . "'><button type='submit' name='editar_cliente'>Editar</button></a>";
+            #Botón para editar el cliente
+            echo "<a href='clientes.php?section=editar&cliente_id=" . $row["idClientes"] . "'><button type='button' name='editar_cliente'>Editar</button></a>";
             
-            // Botón para eliminar el cliente
+            #Botón para eliminar el cliente
             echo "<form method='POST' style='display:inline-block;'>";
             echo "<input type='hidden' name='cliente_id' value='" . $row["idClientes"] . "'>";
             echo "<button type='submit' name='eliminar_cliente'>Eliminar</button>";
@@ -74,27 +101,23 @@ if ($section == 'clientes') {
     }
 }
 
-// Acción de eliminar cliente
+#Acción de eliminar cliente
 if (isset($_POST['eliminar_cliente'])) {
     $cliente_id = $_POST['cliente_id'];
-
-    // Eliminar de la tabla de clientes
     $sql_delete_cliente = "DELETE FROM clientes WHERE idClientes = ?";
     $stmt_delete_cliente = $conn->prepare($sql_delete_cliente);
     $stmt_delete_cliente->bind_param("i", $cliente_id);
     $stmt_delete_cliente->execute();
     $stmt_delete_cliente->close();
 
-    // Recargar la página
+    #Recargar la página
     header("Location: clientes.php?section=clientes");
     exit();
 }
 
-// Acción de editar cliente
+#Acción de editar cliente
 if ($section == 'editar' && isset($_GET['cliente_id'])) {
     $cliente_id = $_GET['cliente_id'];
-
-    // Obtener los datos actuales del cliente
     $sql_editar_cliente = "SELECT idClientes, Nombre, Apellido, Email, Direccion, Telefono, DNI FROM clientes WHERE idClientes = ?";
     $stmt_editar_cliente = $conn->prepare($sql_editar_cliente);
     $stmt_editar_cliente->bind_param("i", $cliente_id);
@@ -104,7 +127,6 @@ if ($section == 'editar' && isset($_GET['cliente_id'])) {
     if ($result_editar_cliente->num_rows == 1) {
         $row = $result_editar_cliente->fetch_assoc();
     
-        // Mostrar formulario para editar cliente
         echo "<div class='edit-client-form'>";
         echo "<h2>Editar Cliente</h2>";
         echo "<form method='POST'>";
@@ -148,6 +170,7 @@ if ($section == 'editar' && isset($_GET['cliente_id'])) {
         echo "<input type='hidden' name='cliente_id' value='" . $row['idClientes'] . "'>";
         
         echo "<div class='button-container'>";
+
         echo "<button type='submit' name='actualizar_cliente'>Actualizar Cliente</button>";
         echo "<button type='button' onclick='window.location.href=\"clientes.php?section=clientes\"'>Cancelar</button>";
         echo "</div>";
@@ -160,7 +183,7 @@ if ($section == 'editar' && isset($_GET['cliente_id'])) {
     $stmt_editar_cliente->close();
 }
 
-// Acción para actualizar cliente
+#Acción para actualizar cliente
 if (isset($_POST['actualizar_cliente'])) {
     $cliente_id = $_POST['cliente_id'];
     $nombre = $_POST['Nombre'];
@@ -170,19 +193,21 @@ if (isset($_POST['actualizar_cliente'])) {
     $telefono = $_POST['Telefono'];
     $dni = $_POST['DNI'];
 
-    // Actualizar el cliente en la base de datos
-    $sql_actualizar_cliente = "UPDATE clientes SET Nombre = ?, Apellido = ?, Email = ?, Direccion = ?, Telefono = ?, DNI = ? WHERE idClientes = ?";
-    $stmt_actualizar_cliente = $conn->prepare($sql_actualizar_cliente);
-    $stmt_actualizar_cliente->bind_param("ssssssi", $nombre, $apellido, $email, $direccion, $telefono, $dni, $cliente_id);
-    $stmt_actualizar_cliente->execute();
-    $stmt_actualizar_cliente->close();
+    $sql_update_cliente = "UPDATE clientes SET Nombre = ?, Apellido = ?, Email = ?, Direccion = ?, Telefono = ?, DNI = ? WHERE idClientes = ?";
+    $stmt_update_cliente = $conn->prepare($sql_update_cliente);
+    $stmt_update_cliente->bind_param("ssssssi", $nombre, $apellido, $email, $direccion, $telefono, $dni, $cliente_id);
+    $stmt_update_cliente->execute();
+    $stmt_update_cliente->close();
 
-    // Recargar la página después de actualizar
+    #Recargar la página
     header("Location: clientes.php?section=clientes");
     exit();
 }
 
-echo "</div>"; // Cierre del div content
+#cerrar la conexión a la base de datos
+$conn->close();
+
+echo "</div>";
 echo "</body>";
 echo "</html>";
 ?>
