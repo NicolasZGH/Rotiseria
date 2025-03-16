054c0cd0a3177cc955886e42e4073cad20de0c27
NicolasZGH <147265658+NicolasZGH@users.noreply.github.com>
2025-03-16 10:25:07 -0300

    corrección cierre de sesión

--- total.php
+++ total.php
@@ -52,7 +52,7 @@ echo "<a href='pedidos.php?section=pedidos'>Agregar Pedidos</a>";
 echo "<a href='total.php?section=total' class='active'>Total de Pedidos</a>";
 echo "<a href='clientes.php?section=clientes'>Clientes</a>";
 echo "<a href='productos.php?section=productos'>Productos</a>";
-echo "<a href='index.php' style='float:right;'>Cerrar sesión</a>";
+echo "<a href='logout.php' style='float:right;'>Cerrar sesión</a>";
 echo "</div>";
 
 echo "<h1>Total de Pedidos</h1>";
